37eb19994e71ddff319c465627894dd8c90ca63b
Ddmitro-ZH <zele_jj21@student.itstep.org>
2025-05-20 15:25:23 +0300

    завершено

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #завдання 2
 
-
+while True:
+    try:
+        number = round(float(input("введіть число ")))
+    except (ValueError):
+        print("ви уввели неправильне число")
+    else:
+        print(number)
+        break
